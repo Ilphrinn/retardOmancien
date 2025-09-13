a7b9e4730fb07e3b1e331caaab9abc44bc4b7f54
Ilphrinn <gitlab.hurled132@passmail.net>
2025-09-13 18:27:32 +0200

    Commit global

--- retard.js
+++ retard.js
@@ -6,14 +6,17 @@ const client = new Client({ intents });
 const buildMessageHandler = require('./src/handlers/messageHandler');
 const buildInteractionHandler = require('./src/handlers/interactionHandler');
 
-// charge automatiquement les commandes du dossier
 const fs = require('fs');
 const path = require('path');
 const commands = new Map();
-const commandsDir = path.join(__dirname, 'commands');
+
+const commandsDir = path.join(__dirname, 'src', 'commands');
+
 for (const file of fs.readdirSync(commandsDir).filter(f => f.endsWith('.js'))) {
   const cmd = require(path.join(commandsDir, file));
-  if (cmd?.name && typeof cmd.execute === 'function') commands.set(cmd.name, cmd);
+  if (cmd?.name && typeof cmd.execute === 'function') {
+    commands.set(cmd.name, cmd);
+  }
 }
 
 client.on('messageCreate', buildMessageHandler(client, triggers));

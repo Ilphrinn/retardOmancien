603bf4c626447407e798188bebdff715ef819731
Ilphrinn <72984329+Ilphrinn@users.noreply.github.com>
2025-10-31 12:46:00 +0000

    Update clanker.js

--- src/commands/clanker.js
+++ src/commands/clanker.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = async function clanker(message) {
   try {
     const imgPath = path.join(__dirname, '..', '..', 'ressources', 'clanker.png');
-    await message.channel.send({ files: [imgPath] });
+    await message.reply({ files: [imgPath] });
   } catch (err) {
     console.error("Erreur en envoyant l'image clanker:", err);
-    try { await message.channel.send("Impossible d'envoyer l'image clanker."); } catch (_) {}
+    try { await message.reply("Gnnneeeuuuuu j'arriv pu a t'insulter tes morts"); } catch (_) {}
   }
 };
